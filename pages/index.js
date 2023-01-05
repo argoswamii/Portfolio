45553be7fadfcb6aaf45b67a1505f7380fdb8a65
abhishek raj goswami <goswamiabhishekraj@gmail.com>
2023-01-05 12:50:13 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {BsGithub,BsLinkedin} from 'react-icons/bs'
+import Image from "next/image";
+import abhi from "../public/profile.png"
 
 
 export default function Home() {
@@ -14,12 +17,24 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Abhi</h1>
+            <h1 className="font-burtons text-xl">Abhishek</h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill/></li>
-              <li><a href='#'>Resume</a></li>
+              <li   className=" cursor-pointer text-2xl"><BsFillMoonStarsFill/></li>
+              <li  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"><a href='#'>Resume</a></li>
             </ul>
           </nav>
+          <div  className="text-center p-10 py-10">
+            <h2  className='text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl'>Abhishek Raj Goswami</h2>
+            <h3 className='text-2xl py-2 dark:text-gray-600 md:text-3xl'>Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-900 max-w-xl mx-auto md:text-xl" >i am a Developer</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-800">
+          <BsGithub/>
+          <BsLinkedin/>
+          </div>
+          <div className="mx-auto bg-gradient-to-b from-teal-400 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <Image src={abhi} layout='fill'/>
+          </div>
         </section>
       </main>
     </div>
